a0845e3fc28e9a9ce41e230562e2e9420c1536ff
nil-kross <nil.kross@gmail.com>
2018-11-29 20:14:54 +0300

    Start implementing Application class.

---
+++ New/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SW = SolidWorks.Interop.sldworks.SldWorks;
+
+namespace Course
+{
+    public class Application
+    {
+        private SW solidWorks = null;
+
+        public Application()
+        {
+        }
+
+        public void Start()
+        {
+            this.solidWorks = SolidWorksApi.GetSolidWorks();
+
+            if (this.solidWorks != null)
+            {
+                ;
+            }
+        }
+    }
+}
---
+++ New/SolidWorksApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using SW = SolidWorks.Interop.sldworks.SldWorks;
+
+namespace Course
+{
+    public static class SolidWorksApi
+    {
+        private static String swApplicationNameString = "SldWorks.Application";
+        public static SW GetSolidWorks()
+        {
+            SW solidWorks = null;
+
+            try
+            {
+                solidWorks = (SW)Marshal.GetActiveObject(SolidWorksApi.swApplicationNameString);
+            }
+            catch { }
+
+            return solidWorks;
+        }
+    }
+}
