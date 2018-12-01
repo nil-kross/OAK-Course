a71067d58e86678165cb3711658855762cffaa2d
nil-kross <nil.kross@gmail.com>
2018-12-01 11:37:39 +0300

    Add Unit class.

---
+++ New/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course {
+    public abstract class Unit {
+        private static String folderPathwayString = "Units";
+
+        public abstract String FileName { get; }
+        public String FileNamePathway {
+            get {
+                return String.Format(@"{0}\{1}.SLDPRT", Unit.folderPathwayString, this.FileName);
+            }
+        }
+    }
+}
