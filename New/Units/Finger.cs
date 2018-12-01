4ec558a7326aaf6e6d81a2e1d2ef2c96ee41df9b
nil-kross <nil.kross@gmail.com>
2018-12-01 18:29:26 +0300

    Add Finger class.

---
+++ New/Units/Finger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Units {
+    public class Finger : Unit {
+        public override String FileName {
+            get => "Locator1";
+        }
+    }
+}
