f8a80331fa8d2f732b79b5427705c49d58cbb410
nil-kross <nil.kross@gmail.com>
2018-12-04 15:00:56 +0300

    Add CustomUnit class.

---
+++ New/Components/CustomUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Components
+{
+    public class CustomUnit : Unit
+    {
+        private readonly String fileNameString = null; 
+
+        public override string FileName => this.fileNameString;
+
+        public CustomUnit(String fileName)
+        {
+            this.fileNameString = fileName;
+        }
+    }
+}
--- New/Components/Unit.cs
+++ New/Components/Unit.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Course.Components {
     public abstract class Unit : Component {
-        protected override String FolderPathwayString => "Units";
+        protected override String FolderPathwayString => @"L:\3 Repositories\OAK-Course\New\bin\Debug\Units"; // TO DO: Replace with relative pathway
     }
 }
