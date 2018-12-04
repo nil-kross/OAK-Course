a7d8608b3d4a96a5d0e0a236757486201a639f6a
nil-kross <39334605+nil-kross@users.noreply.github.com>
2018-12-04 23:21:11 +0300

    Improve Point class.

--- New/Api/Point.cs
+++ New/Api/Point.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace Course.Api {
     public class Point {
+        private readonly Double[] valuesArray = null;
+
         public Double X { get; set; }
         public Double Y { get; set; }
         public Double Z { get; set; }
 
-        public Point(Double x = 0, Double y = 0, Double z = 0) {
+        public Double this[Int32 index] {
+            get {
+                if (index >= 0 && index < this.valuesArray.Length) {
+                    return this.valuesArray[index];
+                } else {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set {
+                if (index >= 0 && index < this.valuesArray.Length) {
+                    this.valuesArray[index] = value;
+                } else {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public static Int32 AxisesCount => 3;
+
+        public Point() {
+            this.valuesArray = new Double[3] { 0, 0, 0 };
+        }
+
+        public Point(Double x, Double y, Double z) : this() {
             this.X = x;
             this.Y = y;
             this.Z = z;
